c3667ff40285991c8e5dafee7dbd4fc4233be13f
Cesar Rodriguez <cesar.rodriguez@lipn.fr>
2017-01-08 16:59:22 +0100

    Getting ready

---
+++ ai-unf/findmax/README.rst
@@ -0,0 +1,16 @@
+
+This is the example in Fig. 1 of the POPL'17 submission. The producer sends
+sorted numbers into a fifo, and the consumer consumes then in order, storing
+them into a sorted array. THe send and recv operations of the fifo have
+spinlocks, which should be ok if we have cutoffs.
+
+main1.c is the very first version I implemented, without testing; it produces
+only 0 in the sorted version
+
+main2.c is after I realize the bug in queue_extract, I need to first decrement
+the size and then access the last. But there is still another bug!
+
+main3.c is after I realize that I need to extract the first of the queue and
+not the last, it is a fifo, not a stack!
+
+main.c == main3.c, modulo better printfs
---
+++ ai-unf/findmax/main2.c
@@ -0,0 +1,148 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define PRINTF2_NORMAL
+#include "printf2.h"
+
+#define MAX_QUEUE 5
+#define MAX_ITEMS 7
+
+int q[MAX_QUEUE];
+int qsiz;
+pthread_mutex_t mq;
+
+void queue_init ()
+{
+   pthread_mutex_init (&mq, NULL);
+   qsiz = 0;
+}
+
+void queue_insert (int x)
+{
+   int done = 0;
+   int i = 0;
+   printf2 ("prod: trying\n");
+   while (done == 0)
+   {
+      i++;
+      pthread_mutex_lock (&mq);
+      if (qsiz < MAX_QUEUE)
+      {
+         printf2 ("prod: got it! x %d qsiz %d i %d\n", x, qsiz, i);
+         done = 1;
+         q[qsiz] = x;
+         qsiz++;
+      }
+      pthread_mutex_unlock (&mq);
+   }
+}
+
+int queue_extract ()
+{
+   int done = 0;
+   int x, i = 0;
+   printf2 ("consumer: trying\n");
+   while (done == 0)
+   {
+      i++;
+      pthread_mutex_lock (&mq);
+      if (qsiz > 0)
+      {
+         done = 1;
+         qsiz--; // otherwise you always read 0!!!
+         x = q[qsiz];
+         printf2 ("consumer: got it! x %d qsiz %d i %d\n", x, qsiz, i);
+      }
+      pthread_mutex_unlock (&mq);
+   }
+   return x;
+}
+
+void swap (int *t, int i, int j)
+{
+   int aux;
+   aux = t[i];
+   t[i] = t[j];
+   t[j] = aux;
+}
+
+int findmax (int *t, int count)
+{
+   int i, mx;
+   mx = 0;
+   for (i = 1; i < count; i++)
+   {
+      if (t[i] > t[mx]) mx = i;
+   }
+   swap (t, mx, count-1);
+   return t[count-1];
+}
+
+int source[MAX_ITEMS];
+int sorted[MAX_ITEMS];
+
+void producer ()
+{
+   int i, max;
+
+   for (i = 0; i < MAX_ITEMS; i++)
+   {
+      max = findmax (source, MAX_ITEMS - i);
+      queue_insert (max);
+   }
+}
+
+void consumer ()
+{
+   int i, max;
+   for (i = 0; i < MAX_ITEMS; i++)
+   {
+      max = queue_extract ();
+      sorted[i] = max;
+   }
+}
+
+void *thread (void * arg)
+{
+   (void) arg;
+   producer ();
+   return NULL;
+}
+
+int main ()
+{
+   pthread_t t;
+
+   __libc_init_poet ();
+
+#if 1
+   // this code initializes the source array with random numbers
+   unsigned seed = (unsigned) time (0);
+   int i;
+   srand (seed);
+   printf ("Using seed %u\n", seed);
+   for (i = 0; i < MAX_ITEMS; i++)
+   {
+      source[i] = random() % 20;
+      assert (source[i] >= 0);
+      printf2 ("source[%d] = %d\n", i, source[i]);
+   }
+   printf2 ("==============\n");
+#endif
+
+   queue_init ();
+   pthread_create (&t, NULL, thread, NULL);
+   consumer ();
+   pthread_join (t, NULL);
+
+#if 1
+   // this code prints the sorted array
+   printf2 ("==============\n");
+   for (i = 0; i < MAX_ITEMS; i++)
+      printf2 ("sorted[%d] = %d\n", i, sorted[i]);
+#endif
+   return 0;
+}
+
